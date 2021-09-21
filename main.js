1005b11e029963eea92de9c5af83cb319fecd0ab
EC2 Default User <ec2-user@ip-172-31-85-180.ec2.internal>
2021-09-21 15:37:09 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+    let result = document.getElementById("result");
+    function edit(elem) {
+      result.value = result.value + elem.value;
+    }
+    
+    function calc() {
+      result.value = new Function("return " + result.value)();
+    }
+    
+    function clear(){
+      $(".monitor").text("");
+    }
